264166adff051941f657736c8451bbafe454187a
Shakeeb Zacky <68572190+ShakeebZ@users.noreply.github.com>
2026-04-07 19:49:04 -0700

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.cpp
@@ -0,0 +1,71 @@
+struct Node {
+    Node* prev = NULL;
+    Node* next = NULL;
+    int value = -1;
+    int key = -1;
+};
+
+class LRUCache {
+    std::unordered_map<int, Node*> lru;
+    Node* head = NULL;
+    Node* tail = NULL;
+    int size = -1;
+public:
+    LRUCache(int capacity) {
+        size = capacity; // account for sentinels
+        Node* n = new Node;
+        Node* t = new Node;
+        head = n;
+        tail = t;
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if (lru.contains(key)) {
+            Node* curr = lru[key];
+            if (head->next != curr) {
+                remove(curr);
+                add(curr);
+            }
+            return curr->value;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if (lru.contains(key)) {
+            Node* temp = lru[key];
+            temp->value = value;
+            remove(temp);
+            add(temp);
+            return;
+        }
+
+        Node* n = new Node();
+        n->value = value;
+        n->key = key;
+
+        add(n);
+        lru[n->key] = n;
+        if (lru.size() > size) {
+            lru.erase(tail->prev->key);
+            remove(tail->prev);
+        }
+    }
+
+    void remove(Node* curr) {
+        Node* prev = curr->prev;
+        Node* next = curr->next;
+        prev->next = next;
+        next->prev = prev;
+    }
+
+    void add(Node* curr) {
+        Node* oldHead = head->next;
+        oldHead->prev = curr;
+        head->next = curr;
+        curr->next = oldHead;
+        curr->prev = head;
+    }
+};
